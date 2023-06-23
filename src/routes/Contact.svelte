984d6f570c4df08668dcad177820315b3f1c774d
Jon Jampen <jon.jampen@cryptography.ch>
2023-06-23 16:17:41 +0200

    Form validation

--- src/routes/Contact.svelte
+++ src/routes/Contact.svelte
@@ -1,11 +1,30 @@
+<script>
+	import { onMount } from 'svelte';
+	let name,
+		email,
+		message,
+		nameClass = 'valid',
+		emailClass = 'valid',
+		messageClass = 'valid',
+		disabled = true,
+		title = 'Please fill out all fields';
+
+	$: name ? (nameClass = 'valid') : (nameClass = '');
+	$: email && email.includes('@') ? (emailClass = 'valid') : (emailClass = '');
+	$: message ? (messageClass = 'valid') : (messageClass = '');
+
+	$: nameClass == 'valid' && emailClass == 'valid' && messageClass == 'valid'
+		? ((disabled = false), (title = 'send message'))
+		: ((disabled = true), (title = 'Please fill out all fields'));
+</script>
+
 <section class="contactForm">
 	<h2>Contact</h2>
 	<h6>asdf</h6>
-
 	<form action="">
-		<input type="text" name="name" id="nameField" />
-		<input type="email" name="email" id="emailField" />
-		<textarea name="message" id="messageField" />
-		<button class="primary">Send</button>
+		<input type="text" name="name" id="nameField" bind:value={name} class={nameClass} />
+		<input type="email" name="email" id="emailField" bind:value={email} class={emailClass} />
+		<textarea name="message" id="messageField" bind:value={message} class={messageClass} />
+		<button class="primary" {disabled} {title}>Send</button>
 	</form>
 </section>
