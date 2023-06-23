dafab4f1c387952cca6d492a21e853eb5a8b750f
Jon Jampen <jon.jampen@cryptography.ch>
2023-06-23 16:24:41 +0200

    Advanced validation

--- src/routes/Contact.svelte
+++ src/routes/Contact.svelte
@@ -1,5 +1,4 @@
 <script>
-	import { onMount } from 'svelte';
 	let name,
 		email,
 		message,
@@ -7,24 +6,51 @@
 		emailClass = 'valid',
 		messageClass = 'valid',
 		disabled = true,
-		title = 'Please fill out all fields';
+		title = 'Please fill out all fields',
+		preclick = true;
 
-	$: name ? (nameClass = 'valid') : (nameClass = '');
-	$: email && email.includes('@') ? (emailClass = 'valid') : (emailClass = '');
-	$: message ? (messageClass = 'valid') : (messageClass = '');
+	$: name || preclick ? (nameClass = 'valid') : (nameClass = '');
+	$: (email && email.includes('@') && email.includes('.')) || preclick
+		? (emailClass = 'valid')
+		: (emailClass = '');
+	$: message || preclick ? (messageClass = 'valid') : (messageClass = '');
 
-	$: nameClass == 'valid' && emailClass == 'valid' && messageClass == 'valid'
+	$: nameClass == 'valid' && emailClass == 'valid' && messageClass == 'valid' && !preclick
 		? ((disabled = false), (title = 'send message'))
 		: ((disabled = true), (title = 'Please fill out all fields'));
+
+	function changeInput(e) {
+		preclick = false;
+	}
 </script>
 
 <section class="contactForm">
 	<h2>Contact</h2>
 	<h6>asdf</h6>
 	<form action="">
-		<input type="text" name="name" id="nameField" bind:value={name} class={nameClass} />
-		<input type="email" name="email" id="emailField" bind:value={email} class={emailClass} />
-		<textarea name="message" id="messageField" bind:value={message} class={messageClass} />
+		<input
+			type="text"
+			name="name"
+			id="nameField"
+			bind:value={name}
+			class={nameClass}
+			on:keyup={changeInput}
+		/>
+		<input
+			type="email"
+			name="email"
+			id="emailField"
+			bind:value={email}
+			class={emailClass}
+			on:keyup={changeInput}
+		/>
+		<textarea
+			name="message"
+			id="messageField"
+			bind:value={message}
+			class={messageClass}
+			on:keyup={changeInput}
+		/>
 		<button class="primary" {disabled} {title}>Send</button>
 	</form>
 </section>
