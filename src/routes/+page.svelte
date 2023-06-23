ae9dffeadd5fdbf24b05c62e745128577d348566
Jon Jampen <jon.jampen@cryptography.ch>
2023-06-23 15:27:08 +0200

    Adding contact form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,16 @@
 	import Footer from './Footer.svelte';
 	import Hero from './Hero.svelte';
 	import Projects from './Projects.svelte';
+	import Contact from './Contact.svelte';
 </script>
 
 <Hero />
 <Projects />
 <!-- Education -->
 <!-- Contact -->
+<section class="contact">
+	<Contact />
+	<img src="/fatiguediary.png" alt="" />
+</section>
 <!-- Footer -->
 <Footer />
---
+++ src/routes/Contact.svelte
@@ -0,0 +1,11 @@
+<section class="contactForm">
+	<h2>Contact</h2>
+	<h6>asdf</h6>
+
+	<form action="">
+		<input type="text" name="name" id="nameField" />
+		<input type="email" name="email" id="emailField" />
+		<textarea name="message" id="messageField" />
+		<button class="primary">Send</button>
+	</form>
+</section>
