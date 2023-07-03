5c2231249ce297840ab164aa3ca9409cf9de9434
Jon Jampen <jon.jampen@cryptography.ch>
2023-07-03 13:26:52 +0200

    Adding svelte-multiselect

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,10 +1,19 @@
 <script>
 	import ProjectCarousel from '../ProjectCarousel.svelte';
+	import MultiSelect from 'svelte-multiselect';
+
+	const ui_libs = [`Svelte`, `React`, `Vue`, `Angular`, `...`];
+
+	let selected = [];
 </script>
 
 <section class="projects-grid">
 	<h2>My Projects</h2>
 	<h6 style="display: none;">Filtered by:</h6>
+	<br />
+
+	<MultiSelect bind:selected options={ui_libs} />
+	<code>selected = {JSON.stringify(selected)}</code>
 	<br />
 	<ProjectCarousel />
 </section>
