faffa367dd529ab3e07fb082790257c18243f1a8
Jon Jampen <jon.jampen@cryptography.ch>
2023-07-03 10:18:38 +0200

    Adding projects grid in projects route

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,8 +2,9 @@
 	import ProjectCarousel from '../ProjectCarousel.svelte';
 </script>
 
-<section class="projects">
+<section class="projects-grid">
 	<h2>My Projects</h2>
 	<h6 style="display: none;">Filtered by:</h6>
+	<br />
 	<ProjectCarousel />
 </section>
