ae162f8fd2162c208f916f4ab7fc0ac268ff3694
Jon Jampen <jon.jampen@cryptography.ch>
2023-07-03 10:20:21 +0200

    Smooth scrolling to contact on main page

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -3,6 +3,6 @@
 	<ul>
 		<li><a href="/">Home</a></li>
 		<li><a href="/projects">Projects</a></li>
-		<li><a href="#contact">Contact</a></li>
+		<li><a href="/#contact">Contact</a></li>
 	</ul>
 </nav>
