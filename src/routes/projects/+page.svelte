295534001b674eb5ef5ded9408cdae38a9d7675b
Jon Jampen <jon.jampen@cryptography.ch>
2023-07-12 10:04:31 +0200

    Adding multiselect tags

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,7 +2,34 @@
 	import ProjectCarousel from '../ProjectCarousel.svelte';
 	import MultiSelect from 'svelte-multiselect';
 
-	const ui_libs = [`Svelte`, `React`, `Vue`, `Angular`, `...`];
+	const tags = [
+		'HTML',
+		'CSS',
+		'JavaScript',
+		'PHP',
+		'Svelte',
+		'SvelteKit',
+		'Svelte',
+		'React',
+		'ReactKit',
+		'Next.js',
+		'MySQL',
+		'Firebase',
+		'SvelteKit',
+		'Svelte',
+		'React',
+		'ReactKit',
+		'Next.js',
+		'MySQL',
+		'Firebase',
+		'SvelteKit',
+		'Svelte',
+		'React',
+		'ReactKit',
+		'Next.js',
+		'MySQL',
+		'Firebase'
+	];
 
 	let selected = [];
 </script>
@@ -11,18 +38,25 @@
 	<h2>My Projects</h2>
 	<h6 style="display: none;">Filtered by:</h6>
 	<br />
-
-	<MultiSelect
-		--sms-border="none"
-		--sms-bg="var(--primary-transparent)"
-		--sms-border-radius="8px"
-		--sms-options-bg="var(--primary-semi-transparent)"
-		--sms-selected-bg="var(--primary-transparent)"
-		--sms-remove-btn-hover-color="var(--primary)"
-		bind:selected
-		options={ui_libs}
-	/>
-	<code>selected = {JSON.stringify(selected)}</code>
+	<div class="filters">
+		<input type="text" class="filter" placeholder="Search..." />
+		<MultiSelect
+			--sms-width="100%"
+			--sms-max-width="500px"
+			class="filter"
+			--sms-border="none"
+			--sms-bg="var(--primary-transparent)"
+			--sms-border-radius="8px"
+			--sms-options-bg="var(--primary-semi-transparent)"
+			--sms-selected-bg="var(--primary-transparent)"
+			--sms-remove-btn-hover-color="var(--primary)"
+			--sms-placeholder-color="var(--gray)"
+			bind:selected
+			options={tags}
+			placeholder="Select tags"
+		/>
+		<button class="btn secondary">search</button>
+	</div>
 	<br />
 	<ProjectCarousel />
 </section>
