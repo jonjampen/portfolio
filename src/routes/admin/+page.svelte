41eb6ef47b7a1bacf5206194264b498dd70a3a19
Jon Jampen <jon.jampen@cryptography.ch>
2023-07-13 15:09:56 +0200

    Admin panel to add projects

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { onMount } from 'svelte';
+	import { db } from '../../lib/firebase';
+	import { collection, addDoc } from 'firebase/firestore';
+
+	async function addProject(e) {
+		console.log(e.target.title.value);
+		let inputs = e.target;
+		let project = {
+			title: inputs.title.value,
+			links: {
+				github: inputs.linkGH.value,
+				website: inputs.linkWEB.value
+			},
+			date: inputs.date.value,
+			description: inputs.shortDesc.value,
+			body: inputs.body.value
+		};
+		let colRef = collection(db, 'projects');
+		let value = await addDoc(colRef, project);
+		// console.log(value);
+	}
+</script>
+
+<form on:submit|preventDefault={addProject}>
+	<input type="text" name="title" id="" />
+	<input type="text" name="linkGH" id="" />
+	<input type="text" name="linkWEB" id="" />
+	<input type="date" name="date" id="" />
+	<textarea name="shortDesc" id="" cols="30" rows="10" />
+	<textarea name="body" id="" cols="50" rows="10" />
+	<button type="submit">Add Project</button>
+</form>
