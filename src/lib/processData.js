50e782be6c11bab965af1253967c410558e1daba
Jon Jampen <jon.jampen@cryptography.ch>
2023-07-21 10:35:35 +0200

    Style and display name of tags

--- src/lib/processData.js
+++ src/lib/processData.js
@@ -17,11 +17,23 @@ export function processProject(snapshot) {
 export function processTags(snapshot) {
     let tagNames = [];
     let tagIds = [];
+    let tags = [];
 
     snapshot.forEach((doc) => {
         tagNames.push(doc.data().name);
         tagIds[doc.data().name] = doc.id;
+        tags[doc.id] = doc.data().name;
     });
 
-    return [tagNames, tagIds];
+    return [tagNames, tagIds, tags];
+}
+
+export function processTags2(snapshot) {
+    let tags = [];
+
+    snapshot.forEach((doc) => {
+        tags[doc.id] = doc.data().name;
+    });
+
+    return tags;
 }
--- src/routes/projects/[projectSlug]/+page.svelte
+++ src/routes/projects/[projectSlug]/+page.svelte
@@ -1,55 +1,71 @@
 <script>
 	import { onMount } from 'svelte';
 	import { getData } from '../../../lib/getData.js';
-	import { processProjects } from '../../../lib/processData.js';
+	import { processProjects, processTags2 } from '../../../lib/processData.js';
 
 	export let data;
 	let projects = [];
 	let project;
 
+	let tags = [];
+
 	onMount(async () => {
 		projects = processProjects(await getData('projects', data.slug));
 		project = projects[0];
+
+		tags = processTags2(await getData('tags'));
+		console.log(tags);
 	});
 </script>
 
-<div class="content">
-	{#await projects then items}
-		{#each items as project}
-			<div class="titlebar">
-				<div class="title">
-					<h1>{project.title}</h1>
-					<div class="meta">
-						<p>{project.date} - Jon Jampen</p>
-					</div>
-				</div>
-				<div class="icons">
-					<a href={project.links.website} target="_blank">
-						<img src="/icons/external.svg" alt="Live version of the Project" class="icon" />
-					</a>
-					<a href={project.links.github} target="_blank">
-						<img src="/icons/github.svg" alt="Project on GitHub" class="icon" />
-					</a>
-				</div>
-			</div>
-
-			<p>{project.body}</p>
-		{/each}
-	{/await}
-</div>
-
-<div class="right-content">
-	<div class="tags">
+<section class="project-content">
+	<div class="content card">
 		{#await projects then items}
 			{#each items as project}
-				{#each project.tags as tag}
-					<a href="/tags/{tag}" class="tag">{tag}</a>
-				{/each}
+				<div class="titlebar">
+					<div class="title">
+						<h1>{project.title}</h1>
+						<div class="meta">
+							<p>{project.date} • Jon Jampen</p>
+						</div>
+					</div>
+				</div>
+
+				<p>{project.body}</p>
 			{/each}
 		{/await}
 	</div>
 
-	<div class="related">
-		<h3>Related Projects</h3>
+	<div class="right-content">
+		<div class="links card">
+			<h3>Links</h3>
+			{#await projects then items}
+				{#each items as project}
+					<div class="icons">
+						<a href={project.links.website} target="_blank">
+							<img src="/icons/external.svg" alt="Live version of the Project" class="icon" />
+						</a>
+						<a href={project.links.github} target="_blank">
+							<img src="/icons/github.svg" alt="Project on GitHub" class="icon" />
+						</a>
+					</div>
+				{/each}
+			{/await}
+		</div>
+
+		<div class="tags card">
+			<h3>Tags</h3>
+			{#await projects then items}
+				{#each items as project}
+					{#each project.tags as tag}
+						<a href="/tags/{tag}" class="tag">{tags[tag]}</a>
+					{/each}
+				{/each}
+			{/await}
+		</div>
+
+		<div class="related card">
+			<h3>Related Projects</h3>
+		</div>
 	</div>
-</div>
+</section>
