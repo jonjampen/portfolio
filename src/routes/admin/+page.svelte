ccaad495efe20c1e499642e39c347126069d282c
Jon Jampen <jon.jampen@cryptography.ch>
2023-07-22 09:34:57 +0200

    Adding Quill editor.

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -19,7 +19,35 @@
 		let colRef = collection(db, 'projects');
 		let value = await addDoc(colRef, project);
 	}
+
+	let editor;
+
+	export let toolbarOptions = [
+		[{ header: 1 }, { header: 2 }, 'blockquote', 'link', 'image', 'video'],
+		['bold', 'italic', 'underline', 'strike'],
+		[{ list: 'ordered' }, { list: 'ordered' }],
+		[{ align: [] }],
+		['clean']
+	];
+
+	onMount(async () => {
+		const { default: Quill } = await import('quill');
+
+		let quill = new Quill(editor, {
+			modules: {
+				toolbar: toolbarOptions
+			},
+			theme: 'snow',
+			placeholder: 'Write the project text...'
+		});
+	});
 </script>
+
+<div class="no-style">
+	<div class="editor-wrapper">
+		<div bind:this={editor} />
+	</div>
+</div>
 
 <form on:submit|preventDefault={addProject}>
 	<input type="text" name="title" id="" />
@@ -30,3 +58,7 @@
 	<textarea name="body" id="" cols="50" rows="10" />
 	<button type="submit">Add Project</button>
 </form>
+
+<style>
+	@import '../../style/quill.css';
+</style>
