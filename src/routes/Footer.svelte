326bcb2195b536d4940181c131b844a26544e1f8
Jon Jampen <jon.jampen@cryptography.ch>
2023-07-29 14:35:23 +0200

    Customizing icons.

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,3 +1,23 @@
 <section class="footer">
-	<p>&copy;2023 <a href="/">Jon Jampen</a></p>
+	<div class="content">
+		<div class="social">
+			<a href="/links"><span>@jonjampen</span></a>
+			<div class="icons">
+				<a href="https://www.github.com/jonjampen/" target="_blank">
+					<img src="/icons/github-white.svg" alt="GitHub" />
+				</a>
+				<a href="https://www.instagram.com/jonjampen/" target="_blank">
+					<img src="/icons/instagram.svg" alt="Instagram" />
+				</a>
+				<a href="mailto:jon.jampen@cryptography.ch" target="_blank">
+					<img src="/icons/mail.svg" alt="Email" />
+				</a>
+				<a href="https://dev.to/jonjampen" target="_blank">
+					<img src="/icons/devdotto.svg" alt="dev.to" />
+				</a>
+				<a href="/links"><img src="/icons/more.svg" alt="all links" title="all links" /></a>
+			</div>
+		</div>
+		<p>&copy;2023 <a href="/">Jon Jampen</a></p>
+	</div>
 </section>
