b42bad55610bd9d0a43145b7ccdcc0da7f2ab71c
Jon Jampen <jon.jampen@cryptography.ch>
2023-07-30 16:26:33 +0200

    Adding links to link page.

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -10,47 +10,47 @@
 	<br />
 	<div class="cards">
 		<div class="big-cards">
-			<div class="big-card card">
+			<a href="https://www.jonjampen.ch/" target="_blank" class="big-card card">
 				<img src="/icons/website.svg" alt="website" />
 				<p>www.jonjampen.ch</p>
-			</div>
-			<div class="big-card card">
+			</a>
+			<a href="mailto:hey@jonjampen.ch" target="_blank" class="big-card card">
 				<img src="/icons/mail.svg" alt="email" />
 				<p>hey@jonjampen.ch</p>
-			</div>
+			</a>
 		</div>
 
 		<div class="small-cards">
-			<div class="small-card card">
+			<a href="https://www.github.com/jonjampen/" target="_blank" class="small-card card">
 				<img src="/icons/github-white.svg" alt="github" />
 				<p>GitHub</p>
-			</div>
-			<div class="small-card card">
+			</a>
+			<a href="https://www.instagram.com/jonjampen/" target="_blank" class="small-card card">
 				<img src="/icons/instagram.svg" alt="instagram" />
 				<p>Instagram</p>
-			</div>
-			<div class="small-card card">
+			</a>
+			<a href="https://www.dev.to/jonjampen/" target="_blank" class="small-card card">
 				<img src="/icons/devdotto.svg" alt="dev.to" />
 				<p>Dev.to</p>
-			</div>
+			</a>
 		</div>
 		<br />
 		<h3>My Projects</h3>
 		<div class="big-cards">
-			<div class="big-card card">
+			<a href="https://www.fatiguediary.ch/" target="_blank" class="big-card card">
 				<img src="/link-images/fatiguediary.svg" alt="email" />
 				<div class="text">
 					<p class="title">Fatigue Diary</p>
 					<p class="desc">A web app for patients suffering from chronic fatigue</p>
 				</div>
-			</div>
-			<div class="big-card card">
+			</a>
+			<a href="https://cyber-crusade.vercel.app/" target="_blank" class="big-card card">
 				<img src="/link-images/cybercrusade.svg" alt="The Cyber Crusade" />
 				<div class="text">
 					<p class="title">Cyber Crusade</p>
 					<p class="desc">An online version of my favorite card game.</p>
 				</div>
-			</div>
+			</a>
 		</div>
 	</div>
 </section>
