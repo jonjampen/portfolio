b498d512b8e651625ff2400661334f4b4263e4d5
Jon Jampen <jon.jampen@cryptography.ch>
2023-08-01 08:32:30 +0200

    Adding responsive nav.

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,12 +1,27 @@
+<script>
+	function closeNav() {
+		document.getElementById('NavItemsContainer').classList.add('hidden-content');
+		document.getElementById('close').classList.add('hidden');
+		document.getElementById('open').classList.remove('hidden');
+	}
+	function openNav() {
+		document.getElementById('NavItemsContainer').classList.remove('hidden-content');
+		document.getElementById('close').classList.remove('hidden');
+		document.getElementById('open').classList.add('hidden');
+	}
+</script>
+
 <nav>
 	<span><a href="/">Jon Jampen</a></span>
-	<ul>
+	<img src="/icons/close.svg" id="close" on:click={closeNav} class="icon hidden" alt="" />
+	<img src="/icons/menu.svg" id="open" on:click={openNav} class="icon" alt="" />
+	<ul id="NavItemsContainer" class="hidden-content">
 		<li><a href="/">Home</a></li>
 		<li><a href="/projects">Projects</a></li>
 		<li><a href="/#contact">Contact</a></li>
 		<li>
 			<a href="https://www.github.com/jonjampen/" target="_blank">
-				<img src="/icons/github.svg" alt="Project on GitHub" class="icon" />
+				<img src="/icons/github.svg" alt="Project on GitHub" class="icon-link" />
 			</a>
 		</li>
 	</ul>
