b538e80a8decfec5b1694185f1b7ff49b7497ced
Jon Jampen <jon.jampen@cryptography.ch>
2023-08-03 11:42:15 +0200

    Adding favicon and meta data to main page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,52 @@
     import About from "./About.svelte";
     import Projects from "./Projects.svelte";
     import Contact from "./Contact.svelte";
+    import { MetaTags } from "svelte-meta-tags";
 </script>
+
+<MetaTags
+    title="Jon Jampen"
+    description="I'm Jon, an 18-year-old programming enthusiast. I
+    started programming at age 12, and web
+    development has become my passion. Now I'm all
+    set to explore computer science in-depth. This website showcases my projects and
+    provides a way to connect."
+    canonical="https://www.jonjampen.ch/"
+    openGraph={{
+        url: "https://www.jonjampen.ch/",
+        title: "Jon Jampen",
+        description:
+            "I'm Jon, an 18-year-old programming enthusiast. I started programming at age 12, and web development has become my passion. Now I'm all set to explore computer science in-depth. This website showcases my projects and provides a way to connect.",
+        /* images: [
+            {
+                url: "https://www.example.ie/og-image-01.jpg",
+                width: 800,
+                height: 600,
+                alt: "Og Image Alt",
+            },
+            {
+                url: "https://www.example.ie/og-image-02.jpg",
+                width: 900,
+                height: 800,
+                alt: "Og Image Alt Second",
+            },
+            { url: "https://www.example.ie/og-image-03.jpg" },
+            { url: "https://www.example.ie/og-image-04.jpg" },
+        ], */
+        site_name: "Jon Jampen",
+    }}
+    twitter={{
+        cardType: "summary_large_image",
+        title: "Jon Jampen",
+        description:
+            "I'm Jon, an 18-year-old programming enthusiast. I started programming at age 12, and web development has become my passion. Now I'm all set to explore computer science in-depth. This website showcases my projects and provides a way to connect.",
+        image: "https://www.example.ie/twitter-image.jpg",
+        imageAlt: "Jon Jampen",
+    }}
+    facebook={{
+        // appId: "1234567890",
+    }}
+/>
 
 <Hero />
 <About />
