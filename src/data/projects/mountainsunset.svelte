b85792dfbac00db05994a3ce9952be8cd5bc69c7
Jon Jampen <jon.jampen@cryptography.ch>
2023-08-21 08:05:46 +0200

    Updating mountain sunset page.

--- src/data/projects/mountainsunset.svelte
+++ src/data/projects/mountainsunset.svelte
@@ -1,1 +1,8 @@
-<h2>Mountain sunset</h2>
+<img src="/projects/mountainsunset.png" alt="" />
+<h2>Description</h2>
+<p>
+    Customizing my coding environment has always been important to me. Therefore, I decided to
+    create my own theme for my favorite code editor: Visual Studio Code. "Mountain Sunset" is a dark
+    Visual Studio Code theme based on the colors orange and blue. As of mid 2023 it has over 200
+    installs.
+</p>
