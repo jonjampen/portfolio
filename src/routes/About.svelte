5e124bbea225ff36cb9e2a2b7427404e4c996dac
Jon Jampen <jon.jampen@cryptography.ch>
2023-08-23 18:01:24 +0200

    Changing test in about.

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -3,12 +3,12 @@
         <h2>About Me</h2>
 
         <p>
-            Hey, I'm Jon (<i class="ipa">/ j'ɒn /</i>), an 18-year-old programming enthusiast. I
-            caught the coding bug at age 12, and it's been an exciting journey ever since. Web
-            development has become my passion, and with my recent completion of the matura, I'm all
-            set to explore computer science in-depth. This website showcases my
+            Hey, I'm Jon, an 18-year-old programming enthusiast. I caught the coding bug at age 12,
+            and it's been an exciting journey ever since. Web development has become my passion, and
+            with my recent completion of the matura, I'm all set to explore computer science
+            in-depth. This website showcases my
             <a href="/projects" style="color: var(--white)">projects</a>
-            and provides a way to connect.
+            and provides a way to <a href="#contact" style="color: var(--white)">connect</a>.
             <br />
             <br />
             Happy coding!
