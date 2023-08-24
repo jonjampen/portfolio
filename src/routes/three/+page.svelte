aaed2a6e000ab5203ca42de93e4d82c3b9697b2f
Jon Jampen <jon.jampen@cryptography.ch>
2023-08-24 12:44:00 +0200

    Setting up files (scene, camera, etc.) and displaying cube.

---
+++ src/routes/three/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { onMount } from "svelte";
+    import Experience from "./Experience/Experience";
+
+    onMount(() => {
+        const experience = new Experience(document.querySelector(".experience-canvas"));
+    });
+</script>
+
+<div class="experience">
+    <canvas class="experience-canvas" />
+</div>
+
+<style>
+    .experience {
+        position: fixed;
+        width: 100vw;
+        height: 100vh;
+    }
+    .experience-canvas {
+        width: 100%;
+        height: 100%;
+    }
+</style>
