c2d457a68f542ea1eb82cbacb173760abefa4553
Jon Jampen <jon.jampen@cryptography.ch>
2023-09-04 18:14:39 +0200

    Adding analytics.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,10 @@
     import "../global.scss";
     import Navigation from "../components/Navigation.svelte";
     import Footer from "./Footer.svelte";
+    import Analytics from "./analytics.svelte";
 </script>
+
+<Analytics />
 
 <Navigation />
 <slot />
