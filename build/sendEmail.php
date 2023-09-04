39b992cb448ab006390ed2868a26147c59d52bce
Jon Jampen <jon.jampen@cryptography.ch>
2023-09-04 19:40:35 +0200

    Changing text in email.

--- build/sendEmail.php
+++ build/sendEmail.php
@@ -8,13 +8,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // email
     $myEmail = 'jon.jampen@cryptography.ch';
     $subject = 'New Contact Form Submission';
-    $editedMessage = "Name: " . $name . "\n\n Email: " . $email . "\n\n Message: " . $message;
+    $editedMessage = "Name: " . $name . "\nEmail: " . $email . "\nMessage:\n\n" . $message;
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
 
     // confirmation email
     $subjectConf = "Confirmation email: Message to Jon Jampen";
-    $messageConf = "Thank you for your message! I will get back to you as soon as possible. If you have any other remarks, please reply to this email. \n\n" . "Your message: \n\n" . "Name: " . $name . "\n Email: " . $email . "\n Message: \n\n" . $message;
+    $messageConf = "Hello " . $name . "\n\nThank you for your message! I will get back to you as soon as possible. If you have any other remarks, please reply to this email.\n\nBest Regards\nJon Jampen\nhttps://jonjampen.ch\nhello@jonjampen.ch\n\n" . "---\n\n" . "Name: " . $name . "\nEmail: " . $email . "\nMessage: \n\n" . $message;
     $headersConf = "From: $myEmail\r\n";
     $headersConf .= "Reply-To: $myEmail\r\n";
 
