2041441509b01725561ee907ccffc0cc4d5f0327
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-02 13:40:16 +0100

    Adding custom tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    screens: {
+      sm: { 'max': '480px' },
+      md: { 'max': '768px' },
+      lg: { 'max': '976px' },
+      xl: { 'max': '1440px' },
+    },
+    colors: {
+      'background': 'var(--background)',
+      'foreground': 'var(--foreground)',
+      'card': 'var(--card)',
+      'card-foreground': 'var(--card-foreground)',
+      'gray': 'var(--gray)',
+      'gray-foreground': 'var(--gray-foreground)',
+      'primary': 'var(--primary)',
+      'primary-foreground': 'var(--primary-foreground)',
+      'secondary': 'var(--secondary)',
+      'secondary-foreground': 'var(--secondary-foreground)',
+    },
+    fontFamily: {
+      sans: ['Urbanist', 'sans-serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
-
