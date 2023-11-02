b2ef8724dc8db962745d726a933dc662bb24180c
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-02 15:42:56 +0100

    Creating nav bar.

---
+++ src/components/NavItem.svelte
@@ -0,0 +1,5 @@
+<script>
+	export let title, link;
+</script>
+
+<a href={link} class="text-foreground">{title}</a>
---
+++ src/components/Navigation.svelte
@@ -0,0 +1,11 @@
+<script>
+	import NavItems from './NavItems.svelte';
+	import ThemeToggle from './ThemeToggle.svelte';
+	import Avatar from './ui/Avatar.svelte';
+</script>
+
+<nav class="w-full flex justify-between items-center h-16 mb-16">
+	<Avatar />
+	<NavItems />
+	<ThemeToggle />
+</nav>
