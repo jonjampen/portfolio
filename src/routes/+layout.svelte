919ff9ff8cae1001c4c3a4b995b9ec4d9585b89b
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-02 18:18:05 +0100

    Adding footer.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
 	import '../app.css';
+	import Footer from '../components/Footer.svelte';
 	import Navigation from '../components/Navigation.svelte';
 </script>
 
-<div class="flex flex-col items-center w-full">
+<div class="flex flex-col items-center w-full justify-between min-h-screen">
 	<div class="max-w-4xl w-full">
 		<Navigation />
 		<slot />
 	</div>
+	<Footer/>
 </div>
