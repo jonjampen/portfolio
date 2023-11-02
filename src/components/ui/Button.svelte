9f5560280e93b9f57704ddec810b70d29826ce20
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-02 16:37:30 +0100

    Finishing landingpage.

---
+++ src/components/ui/Button.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let styleType, handleClick;
+</script>
+
+<button
+	class="h-10 px-3 rounded-lg"
+	on:click={handleClick}
+	style="color: var(--{styleType}-foreground); background-color: var(--{styleType})"
+>
+	<slot />
+</button>
