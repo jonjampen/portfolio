db0ba5dee9b4706e771b3cb5e5d7e547ddf5bf86
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-02 18:09:57 +0100

    Desiging projects page.

---
+++ src/components/ProjectHorizontalCard.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { ExternalLink, Github } from 'lucide-svelte';
+	import Icon from '../components/ui/Icon.svelte';
+
+	export let title, description, tags, slug, links;
+</script>
+
+<div class="h-40 border-card my-3 flex gap-8 items-start w-[800px]">
+	<img src="/projects/fatiguediary/preview.png" alt="Screenshot of Fatigue Diary" class="h-full bg-card rounded-lg" />
+	<div class="px-3">
+        <h3 class="text-lg text-card-foreground font-medium mb-1">{title}</h3>
+        <div class="flex gap-2 overflow-x-scroll scrollbar-hide">
+            {#each tags as tag}
+                <div class="bg-card px-2 py-1 rounded-full text-card-foreground text-[10px]">{tag}</div>
+            {/each}
+        </div>
+		<p class="text-sm text-card-foreground mb-3 mt-2">
+			{description}
+		</p>
+		<div class="flex justify-between items-center">
+			<a href="/projects/{slug}" class="text-secondary">Read more</a>
+			<div class="flex gap-3 items-center mb-3">
+				<Icon handleClick={() => window.open(links.github, '_blank')}
+					><Github class="w-8 h-8 text-gray hover:text-secondary" /></Icon
+				>
+				<Icon handleClick={() => window.open(links.project, '_blank')}
+					><ExternalLink class="w-8 h-8 text-gray hover:text-secondary" /></Icon
+				>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,99 @@
+<script>
+	import PageTitle from '../../components/ui/PageTitle.svelte';
+	import PageSubTitle from '../../components/ui/PageSubTitle.svelte';
+	import ProjectCard from '../../components/ProjectCard.svelte';
+	import ProjectHorizontalCard from '../../components/ProjectHorizontalCard.svelte';
+
+	let projects = [
+		{
+			main: true,
+			slug: 'fatiguediary',
+			title: 'Fatigue Diary',
+			description:
+				'Fatigue Diary is a web app for patients suffering from chronic fatigue syndrome. It is built with PHP, JavaScript and a MySQL database. This is my matura project.',
+			tags: ['NextJS', 'React', 'Tailwind', 'JavaScript'],
+			links: {
+				github: 'https://github.com/jonjampen/fatiguediary',
+				project: 'https://fatiguediary.ch/'
+			},
+		},
+		{
+			main: true,
+			slug: 'cybercrusade',
+			title: 'Cyber Crusade',
+			description:
+				'The cyber crusade is an online version of my favorite card game. It works with multiple users in real time and stores the data in a Firebase database.',
+			tags: ['SvelteKit', 'Firebase', 'JavaScript'],
+			links: {
+				github: 'https://github.com/jonjampen/cyber-crusade',
+				project: 'https://cyber-crusade.vercel.app/'
+			},
+		},
+		{
+			main: false,
+			slug: 'mandelbrot',
+			title: 'Mandelbrot',
+			description:
+				'A visual representation of the mandelbrot set with the option to zoom, move, change colors, etc. This is a school project created with JavaScript and the p5.js library.',
+			tags: ['JavaScript', 'p5.js', 'OOP'],
+			links: {
+				github: 'https://github.com/jonjampen/mandelbrot',
+				project: 'https://jonjampen.github.io/mandelbrot'
+			},
+		},
+		{
+			main: false,
+			slug: 'solarlog',
+			title: 'Solarlog Dashboard',
+			description:
+				'A dashboard that displays the data from your "Solar- Log Base" solar panels.',
+			tags: ['Svelte', 'API', 'JavaScript'],
+			links: {
+				// github: 'https://github.com/jonjampen/cyber-crusade',
+				// project: 'https://cyber-crusade.vercel.app/'
+			},
+		},
+		{
+			main: false,
+			slug: 'cybercrusade',
+			title: 'Cyber Crusade',
+			description: 'Mountain Sunset VS Code Theme',
+			tags: ['Theme'],
+			links: {
+				github: 'https://github.com/jonjampen/mountain-sunset',
+				project: 'https://marketplace.visualstudio.com/items?itemName=JonJampen.mountain-sunset'
+			},
+		},
+	];
+</script>
+
+<PageTitle>My Projects</PageTitle>
+<PageSubTitle class="mb-8">Projects I’ve built</PageSubTitle>
+<div class="flex gap-8">
+	{#each projects as project}
+		{#if project.main}
+			<ProjectCard
+				title={project.title}
+				description={project.description}
+				tags={project.tags}
+				slug={project.slug}
+				links={project.links}
+			/>
+		{/if}
+	{/each}
+</div>
+
+<PageSubTitle class="mt-16">OTHER PROJECTS</PageSubTitle>
+<div class="flex flex-col gap-8">
+	{#each projects as project}
+		{#if !project.main}
+            <ProjectHorizontalCard
+				title={project.title}
+				description={project.description}
+				tags={project.tags}
+				slug={project.slug}
+				links={project.links}
+			/>
+		{/if}
+	{/each}
+</div>
