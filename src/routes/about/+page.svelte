544a6fa99a73c717d5ec46c7ded4cb94063d43d7
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-02 17:15:45 +0100

    Adding about me and education section.

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+	import PageTitle from '../../components/ui/PageTitle.svelte';
+	import PageSubTitle from '../../components/ui/PageSubTitle.svelte';
+	import Paragraph from '../../components/ui/Paragraph.svelte';
+	import EducationItem from '../../components/EducationItem.svelte';
+</script>
+
+<PageTitle>About Me</PageTitle>
+<PageSubTitle class="mb-6">I write code and create websites</PageSubTitle>
+<Paragraph>
+	Hey, I'm Jon Jampen, an 18-year-old programming enthusiast. Since the age of 12 I’ve been
+	fascinated by the world of computer science. Ever since, I’ve taught myself different parts of
+	computer science, starting with basic web dev, moving on the JavaScript, and then to frameworks
+	like Svelte, React, and NextJs. Over the years I constantly pursued this hobby in my free time,
+	working on projects and enhancing my knowledge.
+	<br /> <br />
+	In the last year of my matura, I had the opportunity to choose computer science as a supplementary
+	subject. This opened the door to frontend frameworks, where I've been exploring technologies like SvelteKit
+	and working on real projects.
+	<br /> <br />
+	One of my most meaningful projects was my matura project, where I developed an app for people dealing
+	with Chronic Fatigue Syndrome. This project is still a work in progress, and I'm dedicated to improving
+	and enhancing it.
+	<br /> <br />
+	With my matura completed this year, I'm eagerly looking ahead to studying computer science at university.
+	I can't wait to discover more aspects of this field.
+	<br /> <br />
+	If you're interested in collaboration, discussions, or simply connecting with a fellow enthusiast,
+	feel free to get in touch or explore my projects.
+	<br /> <br />
+	Jon
+</Paragraph>
+
+<PageSubTitle class="mb-6 mt-16">EDUCATION</PageSubTitle>
+<EducationItem title="Matura" date="2019 - 2023">
+	Major: Physics and applied Mathematics
+	<br />
+	Supplementary Subject: Computer Science
+</EducationItem>
+
+<PageSubTitle class="mb-6 mt-16">CONTACT</PageSubTitle>
