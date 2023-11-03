c921ccd1cd580d8cd8ea4e8a9371a5e77205af4c
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-03 09:56:43 +0100

    Adding responsiveness

--- src/components/EducationItem.svelte
+++ src/components/EducationItem.svelte
@@ -7,7 +7,7 @@
 	<div class="rounded-full h-11 w-11 bg-card flex items-center justify-center text-card-foreground">
 		<BookCopy class="w-7 h-7" />
 	</div>
-	<div class="flex flex-col gap-1 w-full max-w-2xl">
+	<div class="flex flex-col gap-1 w-full sm:w-auto max-w-2xl">
 		<div class="flex justify-between">
 			<h4 class="text-foreground text-lg font-medium">{title}</h4>
 			<h4 class="text-gray text-lg font-light">{date}</h4>
