d41538d2d8906150d17aea8a220e15d6fc1ad4af
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-09 12:11:15 +0100

    Send email contact form with sveltekit-superform.

---
+++ src/routes/about/+page.server.js
@@ -0,0 +1,51 @@
+import { z } from 'zod';
+import { superValidate } from 'sveltekit-superforms/server';
+import { fail } from '@sveltejs/kit';
+import { VITE_BASE_URL } from '$env/static/private';
+
+const schema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    message: z.string(),
+});
+
+export const load = (async () => {
+    const form = await superValidate(schema);
+
+    return { form };
+});
+
+async function sendForm(form) {
+    return await fetch(VITE_BASE_URL + '/sendMail.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            name: form.data.name,
+            email: form.data.email,
+            message: form.data.message,
+        }),
+    });
+}
+
+export const actions = {
+    default: async ({ request }) => {
+        const form = await superValidate(request, schema);
+        console.log('POST', form);
+
+        if (!form.valid) {
+            return fail(400, { form });
+        }
+
+        let response = await sendForm(form);
+        console.log(response)
+        console.log(response.ok)
+        if (!response.ok) {
+            form.errors = { general: ["Email could not be sent. Try again or send an email to hello@jonjampen.ch. Thanks."] }
+            return fail(500, { form })
+        }
+
+        return { form };
+    }
+};
---
+++ static/sendMail.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // email
+    $myEmail = 'hello@jonjampen.ch';
+    $subject = 'New Contact Form Submission';
+    $editedMessage = "Name: " . $name . "\nEmail: " . $email . "\nMessage:\n\n" . $message;
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // confirmation email
+    $subjectConf = "Confirmation email: Message to Jon Jampen";
+    $messageConf = "Hello " . $name . "\n\nThank you for your message! I will get back to you as soon as possible. If you have any other remarks, please reply to this email.\n\nBest Regards\nJon Jampen\nhttps://jonjampen.ch\nhello@jonjampen.ch\n\n" . "---\n\n" . "Name: " . $name . "\nEmail: " . $email . "\nMessage: \n\n" . $message;
+    $headersConf = "From: $myEmail\r\n";
+    $headersConf .= "Reply-To: $myEmail\r\n";
+
+    // send email
+    mail($myEmail, $subject, $editedMessage, $headers);
+
+    // Send confirmation email
+    mail($email, $subjectConf, $messageConf, $headersConf);
+}
