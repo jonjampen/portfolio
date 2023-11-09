afcb905a25fc0db857bc08d0535739ef2b8aa630
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-09 14:39:57 +0100

    New Bitmoji, Testing share page, Error pages.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Avatar from '../components/ui/Avatar.svelte';
+import PageTitle from './../components/ui/PageTitle.svelte';
+</script>
+<section class="flex flex-col items-center justify-center gap-6 text-center">
+    <PageTitle>Page Not Found</PageTitle>
+    <img src="/avatar/searching.png" alt="Avatar of Jon" class="w-64 h-64" />
+    <p class="text-foreground">Sorry, the page you were looking for could not be found. Please go back to the <a href="/">homepage</a> or contact me at <a href="mailto:hello@jonjampen.ch">hello@jonjampen.ch</a>.</p>
+</section>
