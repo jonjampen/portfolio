ba615495f354b2a9db3ae66be81e3cf903c36772
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-11 11:42:56 +0100

    Adding metadata for links.

---
+++ src/routes/links/+page.js
@@ -0,0 +1,11 @@
+export function load() {
+    return {
+        meta: {
+            title: 'Links for Jon Jampen',
+            description: "I'm Jon Jampen, an 18-year-old programming enthusiast. Since the age of 12 I’ve been fascinated by the world of computer science. Now, I enjoy building websites and learning new technologies.",
+            keywords: "Jon Jampen, programming, coding, computer science, web development, portfolio, website, personal website, contact, about, links, social, social media, about me, education, JavaScript, Svelte, React, NextJs, SvelteKit, frontend frameworks, matura project, Chronic Fatigue Syndrome app, portfolio, university, coding projects",
+            canonical: "/links",
+            image: "/avatar/happy.png"
+        }
+    };
+}
