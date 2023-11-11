fb9bfc8a4d145f80f8875d8f96dda8e5febfd6cf
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-11 11:29:53 +0100

    Adding metadata for projects page.

--- src/routes/projects/+page.js
+++ src/routes/projects/+page.js
@@ -7,6 +7,14 @@ export const load = async ({ fetch }) => {
 
     return {
         projects,
-        smallProjects
+        smallProjects,
+        meta: {
+            title: 'Projects by Jon Jampen',
+            description: "Explore a collection of programming projects by Jon Jampen, an 18-year-old computer science enthusiast. From foundational web development to advanced frameworks like Svelte, React, and NextJs, discover a diverse range of projects showcasing my passion for technology.",
+            keywords: "Jon Jampen, programming, coding, computer science, web development, portfolio, website, personal website, projects, work, learning, education, JavaScript, Svelte, React, NextJs, SvelteKit, frontend frameworks, matura project, Chronic Fatigue Syndrome app, portfolio, university, coding projects",
+            canonical: "/projects",
+            image: "/avatar/happy.png"
+        }
+
     };
 };
