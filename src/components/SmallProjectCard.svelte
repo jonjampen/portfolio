134648ba1fc3096d87ab64b79822bb3078862daa
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-11 12:22:43 +0100

    Improving color contrast.

--- src/components/SmallProjectCard.svelte
+++ src/components/SmallProjectCard.svelte
@@ -2,7 +2,7 @@
     export let title, link, image;
 </script>
 
-<a href={link} target="_blank" class="bg-card aspect-[6/4] rounded-lg flex items-center justify-center no-underline relative h-[180px]">
+<a href={link} target="_blank" class="bg-card aspect-[6/4] rounded-lg flex items-center justify-center no-underline relative h-[180px] border border-gray">
     <img src="/images/smallProjects/{image}" alt="" class="absolute w-full h-full rounded-lg object-fill">
     <div class="absolute w-full h-full hover:bg-card-transparent hover:text-card-foreground text-[#00000000] font-bold text-2xl rounded-md transition-all flex items-center justify-center">
         {title}
